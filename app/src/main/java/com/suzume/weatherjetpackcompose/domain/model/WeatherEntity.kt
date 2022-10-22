3d205923bd4a7413951290c83ac411f0e4978034
Aleksei Vorobev <aleksei.suzume@gmail.com>
2022-10-22 23:11:20 +0700

    Feat: refactoring

--- app/src/main/java/com/suzume/weatherjetpackcompose/domain/model/WeatherEntity.kt
+++ app/src/main/java/com/suzume/weatherjetpackcompose/domain/model/WeatherEntity.kt
@@ -1,13 +1,13 @@
 package com.suzume.weatherjetpackcompose.domain.model
 
 data class WeatherEntity(
-    val cityName: String?,
-    val countryName: String?,
-    val currentTime: String?,
-    val currentTemp: String?,
-    val currentIconId: String?,
-    val currentCondition: String?,
-    val currentWindSpeed: String?,
-    val forecasts: Map<Int, Forecast?>?,
-    val hours: List<Hour?>?,
+    val cityName: String,
+    val countryName: String,
+    val currentTime: String,
+    val currentTemp: String,
+    val currentIconId: String,
+    val currentCondition: String,
+    val currentWindSpeed: String,
+    val forecasts: Map<Int, Forecast>,
+    val hours: List<Hour?>,
 )
